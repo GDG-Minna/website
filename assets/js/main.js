26f02860d1619d52ee6e8d61db7ae3ff435dd9ff
shuaibu78 <freezzemx@gmail.com>
2019-11-05 16:23:46 +0100

    api setup updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,67 @@ const offset = el => {
   });
   console.log(elTopPosition);
 };
+
+function prevent(e) {
+  e.preventDefault();
+}
+
+const cards = document.getElementById('cards')
+
+let events = [];
+
+try {
+  const consumeApi = async function apiRequest() {
+    const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.meetup.com/gdgminna/events?sign=true&photo-host=public&page=50&access_token=3b3eab9e6952edaaa8dfded8350e3f0c');
+    const result = await response.json();
+      events = result;
+      console.log(events)
+      showEvents(events);
+  }
+  consumeApi()
+} catch (error) {
+  console.log('error')
+}
+
+function showEvents(evts){
+  let cardTemplate = '';
+  if(evts.length < 1){
+    cardTemplate  = `<h1 class="lg:flex lg:items-center lg:justify-center">There are no event for this page</h1>`
+  }else{
+  evts.forEach(events => {
+    cardTemplate +=`
+    <div class="uk-position-relative uk-visible-toggle uk-light lg:mb-12" tabindex="-1" uk-slider>
+
+      <div class="uk-position-relative uk-visible-toggle uk-light lg:mb-10" tabindex="-1">
+
+        <ul class="uk-slider-items uk-child-width-1-3@s uk-grid">
+          <li>
+            <div class="lg:ml-4 uk-card shadow-none lg:my-2">
+              <div class="uk-card-body">
+                <div class="details rounded-b">
+                  <h3 class="uk-card-title">${events.name}</h3>
+                  <p class="event-text">${events.description.slice(0, 181)}.</p>
+                  <div class="flex justify-between">
+                    <div class="justify-start">
+                      <span class="evt-date">Date</span>
+                      <p class="font-bold timestamp">${events.local_date}</p>
+                    </div>
+                    <div class="justify-end">
+                      <span class="evt-date">Time</span>
+                      <p class="font-bold timestamp">${events.local_time}</p>
+                    </div>
+                  </div>
+                  <div class="mt-2">
+                    <span><a href=${events.link} class="reserve">All seats Booked</a></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>`;
+  })
+}
+cards.innerHTML = cardTemplate;
+}
